723535be0dc2853cd2610bb10a3cefe3074e0770
EC2 Default User <saki1803terashima@gmail.com>
2020-11-29 03:49:55 +0000

    post_finish_bookers2

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
-  resources :book, only:[:new, :create, :index, :show, :destroy]
+  resources :books, only:[:new, :create, :index, :show, :destroy]
 end
