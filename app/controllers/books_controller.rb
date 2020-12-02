5481a5008de479aa3acae6cefc309529c1d6da7a
EC2 Default User <saki1803terashima@gmail.com>
2020-12-02 11:48:26 +0000

    making_bootstrap

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,5 @@
 class BooksController < ApplicationController
-  def new
-  end
+
 
   def create
     @books = Book.all
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  get 'users/show'
   get "abort" => "homes#show"
   devise_for :users
 
