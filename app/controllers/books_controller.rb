c673a9ed2cb2ccb40df43cb6fddc0af03de51470
EC2 Default User <saki1803terashima@gmail.com>
2020-12-20 05:16:06 +0000

    finish2_検索機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -53,6 +53,17 @@ before_action :authenticate_user!
     @book.destroy
     redirect_to books_path, notice: 'Book was successfully delete.'
   end
+  
+  def search
+    @user_or_book = params[:option]
+    @how_seach = params[:choice]
+    if @user_or_book == "1"
+      @users = User.search(params[:search],@user_or_book,@how_seach)
+    elsif @user_or_book == "2"
+      @books = Book.search(params[:search],@user_or_book,@how_seach)
+    else
+    end
+  end
 
 
 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -46,6 +46,17 @@ before_action :authenticate_user!
     @users = @user.followers
     render 'show_follower'
   end
+  
+  def search
+    @user_or_book = params[:option]
+    @how_seach = params[:choice]
+    if @user_or_book == "1"
+      @users = User.search(params[:search],@user_or_book,@how_seach)
+    elsif @user_or_book == "2"
+      @books = Book.search(params[:search],@user_or_book,@how_seach)
+    else
+    end
+  end
 
 
   private
